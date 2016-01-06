7bbea40f2f9447a8c2d01fbb0b45f396a651631b
Daniel Kershaw <danjamker@gmail.com>
2016-01-06 09:52:06 +0000

    Initial Commit

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-python MRLevenshtein.py  -r hadoop -v hdfs://user/kershad1/reddit-innovations -o ./output/1 --wordlist ./Data/words --no-output --conf-path .mrjob.conf
+python MRLevenshtein.py  -r hadoop -v hdfs://scc-culture-mind.lancs.ac.uk/user/kershad1/reddit-innovations -o ./output/1 --wordlist ./Data/words --no-output --conf-path .mrjob.conf
